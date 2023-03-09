f4584b4768dc6d267a4666a5b76651b6ae5e73c5
timmmartinson <timmmartinson@gmail.com>
2023-03-09 12:37:12 -0700

    create base user model

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        // Only hashed PW results are stored
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            transform: (_doc, user) => {
+                // this is so the user's PW is not included in the JSON data
+                delete user.password
+                return user
+            },
+        },
+    }
+)
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.password)
+}
+
+module.exports = mongoose.model('User', userSchema)
