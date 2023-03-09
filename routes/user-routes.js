33c4f480f09cd31254ff105fe3d001b3e4c81dc9
timmmartinson <timmmartinson@gmail.com>
2023-03-09 12:53:33 -0700

    create sign-in/sign-up routes

---
+++ routes/user-routes.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+
+const User = require('../models/user')
+
+const { createUserToken } = require('../config/auth')
+
+const router = express.Router()
+
+// POST /sign-up
+router.post('/sign-up', (req, res, next) => {
+    bcrypt
+        .hash(req.body.credentials.password, 10)
+        .then(hashedPassword => {
+            return {
+                email: req.body.credentials.email,
+                password: hashedPassword
+            }
+        })
+        .then(user => User.create(user))
+        .then(user => {
+            res.status(201).json({ user: user })
+        })
+        .catch(next)
+})
+
+// POST /sign-in
+router.post('/sign-in', (req, res, next) => {
+    User.findOne({ email: req.body.credentials.email })
+    .then((user) => {
+        if (!user) {
+            const error = new Error('The provided email or password is incorrect')
+            error.statusCode = 401
+            throw error
+        }
+
+        if (!user.validatePassword(req.body.credentials.password)) {
+            const error = new Error('The provided email or password is incorrect')
+            error.statusCode = 401
+            throw error
+        }
+
+        const token = createUserToken(req, user)
+        res.status(200).json({ token })
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+module.exports = router
